abfba75a924855f1a4d40c10989c683b335a9bff
jpCaglianone <joao.caglianone@gmail.com>
2024-09-14 16:37:24 -0300

    gateway server side para ligar as urls aos servicos com url dinamica no eureka

---
+++ src/main/java/com/example/gatewaypb/GatewayPbApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewaypb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayPbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gatewaypb/GatewayPbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaypb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
